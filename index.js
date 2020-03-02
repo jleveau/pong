99cae9d1a778b00525c162c55957c08ba7dea043
Tifenn <tifenn.queudet@gmail.com>
2020-03-02 20:28:17 +0100

    le pad et la balle

--- index.js
+++ index.js
@@ -1,42 +1,79 @@
 
 const SPEED = 10;
-let postitionX = 0;
-let postitionY = 0;
+const SPEED_PAD = 15;
+const WIDTH = 800;
+const HEIGTH = 680;
 
-let currentSpeedX = 0;
-let currentSpeedY = 0;
+const BALL_RADIUS = 40;
+const PAD_WIDTH = 50;
+const PAD_HEIGTH = 150;
+
+let positionEllipseX = WIDTH/2;
+let positionEllipseY = HEIGTH/2;
+
+let currentSpeedEllipseX = SPEED;
+let currentSpeedEllipseY = SPEED;
+
+let positionRectX = WIDTH/10;
+let positionRectY = HEIGTH/2 - PAD_HEIGTH/2;
+
+
+let currentSpeedRectY = 0;
 
 
 function setup() {
   frameRate(30);
-  createCanvas(800, 680);
-  
+  createCanvas(WIDTH, HEIGTH);
 }
+
 
 function draw() {
   background(75);
-  
-  if (!(keyIsDown(LEFT_ARROW) || keyIsDown(RIGHT_ARROW))) {
-    currentSpeedX = 0;
-  } 
+
   if (!(keyIsDown(UP_ARROW) || keyIsDown(DOWN_ARROW))) {
-    currentSpeedY = 0;
+    currentSpeedRectY = 0;
   } 
 
-  postitionX += currentSpeedX;
-  postitionY += currentSpeedY;
-  ellipse(postitionX, postitionY, 80, 80);
+
+  positionRectY += currentSpeedRectY;
+
+  positionEllipseX += currentSpeedEllipseX;
+  positionEllipseY += currentSpeedEllipseY;
+ 
+
+  if (positionEllipseX >= (WIDTH - BALL_RADIUS)) {
+    currentSpeedEllipseX = -SPEED;
+  }
+  else if (positionEllipseX <= BALL_RADIUS) {
+    currentSpeedEllipseX = SPEED;
+  }
+
+  else if (positionEllipseY >= (HEIGTH - BALL_RADIUS)) {
+    currentSpeedEllipseY = -SPEED;
+  }
+  else if (positionEllipseY <= BALL_RADIUS) {
+    currentSpeedEllipseY = SPEED;
+  }
+
+  if ((positionEllipseX <= positionRectX + PAD_WIDTH + BALL_RADIUS) && (positionEllipseY >= positionRectY) && (positionEllipseY <= positionRectY + PAD_HEIGTH)) {
+    currentSpeedEllipseX = SPEED;
+  }
+  
+
+  circle(positionEllipseX, positionEllipseY, BALL_RADIUS);
+  rect(positionRectX, positionRectY, PAD_WIDTH, PAD_HEIGTH);
 
 }
 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    currentSpeedX = -SPEED;
-  } else if (keyCode === RIGHT_ARROW) {
-    currentSpeedX = SPEED;
-  } else if (keyCode === UP_ARROW) {
-    currentSpeedY = -SPEED;
+
+    if (keyCode === UP_ARROW) {
+    currentSpeedRectY = -SPEED_PAD;
   } else if (keyCode === DOWN_ARROW) {
-    currentSpeedY = SPEED;
+    currentSpeedRectY = SPEED_PAD;
   }
 }
+
+
+
+
