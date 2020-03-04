eafffd58f2c6e915c8a87bf9c6fe8795897ebfed
Tifenn <tifenn.queudet@gmail.com>
2020-03-04 10:05:13 +0100

    Deux problèmes : je n'arrive pas à chnager le code touche clavier pour le joueur 1 et je n'arrive pas à afficher le texte.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 const SPEED = 10;
 const SPEED_PAD = 15;
+const SPEED_PAD2 = 15;
 const WIDTH = 800;
 const HEIGTH = 680;
 
-const BALL_RADIUS = 40;
+const BALL_RADIUS = 30;
 const PAD_WIDTH = 50;
 const PAD_HEIGTH = 150;
 
@@ -17,8 +18,17 @@ let currentSpeedEllipseY = SPEED;
 let positionRectX = WIDTH/10;
 let positionRectY = HEIGTH/2 - PAD_HEIGTH/2;
 
+let positionRect2X = WIDTH/1.2;
+let positionRect2Y = HEIGTH/2 - PAD_HEIGTH/2;
 
 let currentSpeedRectY = 0;
+let currentSpeedRect2Y = 0;
+
+let scoreJoueur1 = 0;
+let scoreJoueur2 = 0;
+
+let text = 'Score joueur 1 = ' + scoreJoueur1;
+let text2 = 'Score joueur 2 = ' + scoreJoueur2;
 
 
 function setup() {
@@ -34,20 +44,28 @@ function draw() {
     currentSpeedRectY = 0;
   } 
 
+  if (!(keyIsDown(UP_ARROW) || keyIsDown(DOWN_ARROW))) {
+    currentSpeedRect2Y = 0;
+  } 
+
 
   positionRectY += currentSpeedRectY;
+  positionRect2Y += currentSpeedRect2Y;
 
   positionEllipseX += currentSpeedEllipseX;
   positionEllipseY += currentSpeedEllipseY;
  
 
   if (positionEllipseX >= (WIDTH - BALL_RADIUS)) {
-    currentSpeedEllipseX = -SPEED;
+    positionEllipseX = WIDTH/2;
+    positionEllipseY = HEIGTH/2;
+    scoreJoueur1 ++;
   }
   else if (positionEllipseX <= BALL_RADIUS) {
-    currentSpeedEllipseX = SPEED;
+    positionEllipseX = WIDTH/2;
+    positionEllipseY = HEIGTH/2;
+    scoreJoueur2 ++;
   }
-
   else if (positionEllipseY >= (HEIGTH - BALL_RADIUS)) {
     currentSpeedEllipseY = -SPEED;
   }
@@ -58,19 +76,30 @@ function draw() {
   if ((positionEllipseX <= positionRectX + PAD_WIDTH + BALL_RADIUS) && (positionEllipseY >= positionRectY) && (positionEllipseY <= positionRectY + PAD_HEIGTH)) {
     currentSpeedEllipseX = SPEED;
   }
-  
+  else if ((positionEllipseX >= positionRect2X - BALL_RADIUS) && (positionEllipseY >= positionRect2Y) && (positionEllipseY <= positionRect2Y + PAD_HEIGTH)) {
+    currentSpeedEllipseX = -SPEED;
+  }
 
+  
   circle(positionEllipseX, positionEllipseY, BALL_RADIUS);
   rect(positionRectX, positionRectY, PAD_WIDTH, PAD_HEIGTH);
+  rect(positionRect2X, positionRect2Y, PAD_WIDTH, PAD_HEIGTH);
 
 }
 
+ 
 function keyPressed() {
 
     if (keyCode === UP_ARROW) {
     currentSpeedRectY = -SPEED_PAD;
   } else if (keyCode === DOWN_ARROW) {
     currentSpeedRectY = SPEED_PAD;
+  }
+
+  if (keyCode === UP_ARROW) {
+    currentSpeedRect2Y = -SPEED_PAD2;
+  } else if (keyCode === DOWN_ARROW) {
+    currentSpeedRect2Y = SPEED_PAD2;
   }
 }
 
